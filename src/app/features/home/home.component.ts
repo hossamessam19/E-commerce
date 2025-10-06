212899c71459ea7f6bcc4d3e5551ffa03d4f813e
hossamessam19 <7ambozo19@gmail.com>
2025-10-06 11:58:57 +0300

    final

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,7 +1,4 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { ProductsService } from '../../core/services/products/products.service';
-import { Product } from '../../core/models/product.interface';
-import { CardComponent } from '../../shared/components/card/card.component';
 import { MainSliderComponent } from './components/main-slider/main-slider.component';
 import { PopularCategoriesComponent } from './components/popular-categories/popular-categories.component';
 import { PopularProductsComponent } from './components/popular-products/popular-products.component';
@@ -9,7 +6,6 @@ import { PopularProductsComponent } from './components/popular-products/popular-
 @Component({
   selector: 'app-home',
   imports: [
-    CardComponent,
     MainSliderComponent,
     PopularCategoriesComponent,
     PopularProductsComponent,
@@ -17,4 +13,4 @@ import { PopularProductsComponent } from './components/popular-products/popular-
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent { }
