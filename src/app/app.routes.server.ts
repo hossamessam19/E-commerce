14e529709caf29420845e36a71a11968cac0d7ee
hossamessam19 <7ambozo19@gmail.com>
2025-10-06 11:09:29 +0300

    sec

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,14 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+
+  { path: 'details/:slug/:id', renderMode: RenderMode.Server },
+  { path: 'specific-category/:slug/:id', renderMode: RenderMode.Server },
+  { path: 'specific-brand/:slug/:id', renderMode: RenderMode.Server },
+  { path: 'checkout/:id', renderMode: RenderMode.Server },
+  { path: 'details/:id', renderMode: RenderMode.Server },
+  { path: 'specific-category/:id', renderMode: RenderMode.Server },
+  { path: 'specific-brand/:id', renderMode: RenderMode.Server },
   {
     path: '**',
     renderMode: RenderMode.Prerender
